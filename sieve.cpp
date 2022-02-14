20210d8ed329ca2b4788d33a4fdfa1b461999eb5
R0k0n <49793560+R0k0n@users.noreply.github.com>
2022-02-14 18:13:53 +0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,15 +1,14 @@
-        //In main finction
-
-        int n;n = 10000007;
-        vector<bool> is_prime(n+1, true);
+        int nnn;nnn = 10000007;
+        vector<bool> is_prime(nnn+1, true);
         is_prime[0] = is_prime[1] = false;
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i * i <= nnn; i++) {
             if (is_prime[i]) {
-                for (int j = i * i; j <= n; j += i)
+                for (int j = i * i; j <= nnn; j += i)
                     is_prime[j] = false;
             }
         }
         
-        for(ll i = 2 ; i <= n ; i++){
+        for(ll i = 2 ; i <= nnn ; i++){
             if(is_prime[i])primes.push_back(i);
         }
+        
