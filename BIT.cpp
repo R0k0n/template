a0d1439c7c82916c922896e1299cc9ff4e2520a5
R0k0n <49793560+R0k0n@users.noreply.github.com>
2022-05-24 16:47:26 +0600

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,5 +1,7 @@
+ll N = 200005;
 
 ll BIT[200005];
-void update(int x,int val) { ++x;  while(x<=N)  {  BIT[x]+=val  x+=(x&-x);  } }
+void update(int x,int val) { ++x;  while(x<=N)  {  BIT[x]+=val;  x+=(x&-x);  } } /// update increases the value of xth index by val
 
-int query(int x) {  ++x;  int res=0;  while(x>0)  {  res+=BIT[x];  x-=(x&-x);  } return res; } 
+int query(int x) {  ++x;  int res=0;  while(x>0)  {  res+=BIT[x];  x-=(x&-x);  } return res; } /// query returns the sum of range [1 , x]
+
