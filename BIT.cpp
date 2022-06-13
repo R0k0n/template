9b03af1498b466e98c122cdbd72182398d93499e
Rokonuzzaman Rokon <49793560+R0k0n@users.noreply.github.com>
2022-06-13 21:52:38 +0600

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -5,3 +5,4 @@ void update(int x,int val) { ++x;  while(x<=N)  {  BIT[x]+=val;  x+=(x&-x);  } }
 
 int query(int x) {  ++x;  int res=0;  while(x>0)  {  res+=BIT[x];  x-=(x&-x);  } return res; } /// query returns the sum of range [1 , x]
 
+void BITclear(){for(ll i = 0 ; i < N ; i++)BIT[i] = 0;} ///To clear the tree
