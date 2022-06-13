431caae5d8c60d957dc21219b130f0fb4aa12ab7
Rokonuzzaman Rokon <49793560+R0k0n@users.noreply.github.com>
2022-06-13 21:17:42 +0600

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,5 +1,6 @@
         int nnn;nnn = 10000007;
         vector<bool> is_prime(nnn+1, true);
+        vector<int>primes;
         is_prime[0] = is_prime[1] = false;
         for (int i = 2; i * i <= nnn; i++) {
             if (is_prime[i]) {
