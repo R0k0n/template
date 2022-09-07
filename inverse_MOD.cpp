e182b861f3c01eb70470df27be76f1174d24e9d1
Rokonuzzaman Rokon <49793560+R0k0n@users.noreply.github.com>
2022-09-07 20:12:54 +0600

    Create inverse_MOD.cpp

---
+++ inverse_MOD.cpp
@@ -0,0 +1,16 @@
+ll bigmod(ll a, ll b, ll mod) {
+    if (!b) return 1;
+
+    ll x = bigmod(a, b / 2, mod);
+    x = ((x % mod) * (x % mod)) % mod;
+
+    if (b & 1) x = ((x % mod) * (a % mod)) % mod;
+    return x;
+}
+
+// return (1 / a) % b. multiply this with ans.
+ll inverse_mod(ll a, ll b) {
+    ll ret = bigmod(b , MOD - 2 , MOD);
+    ret = (ret * a) % MOD;
+    return ret;
+}
