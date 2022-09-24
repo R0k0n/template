6d2be7d7a82a827583d2975b36ae489e1cd3e3fc
Rokonuzzaman Rokon <49793560+R0k0n@users.noreply.github.com>
2022-09-25 01:37:27 +0600

    Update SparseTable.cpp

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -7,11 +7,7 @@ int P[N][LG];
 
 
 
-for(int i = 1;i <= n;i++) P[i][0] = arr[i];
-for(int i = 1; i < LG; i++){
-        for(int j = 1;j + (1<<i) - 1 <= n;j++)
-                P[j][i] = max(P[j][i - 1] , P[j + (1<<(i - 1))][i - 1]);
-}
+
 
 int query(int l , int r){
   int lg;  
@@ -19,4 +15,11 @@ int query(int l , int r){
   for(lg = 0; (1<<lg) <= len; lg++); lg--;
   //2^lg <= len of that range
   return max(P[l][lg] , P[r - (1<<lg) + 1][lg]);
-} 
+}
+
+
+        for(int i = 1;i <= n;i++) P[i][0] = arr[i];
+        for(int i = 1; i < LG; i++){
+            for(int j = 1;j + (1<<i) - 1 <= n;j++)
+                    P[j][i] = max(P[j][i - 1] , P[j + (1<<(i - 1))][i - 1]);
+        }
