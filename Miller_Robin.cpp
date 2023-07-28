4fb10ff48458a75605896c1e6c22ef9ee638a900
Rokonuzzaman Rokon <49793560+R0k0n@users.noreply.github.com>
2023-07-29 03:39:05 +0600

    Update Miller_Robin.cpp

--- Miller_Robin.cpp
+++ Miller_Robin.cpp
@@ -1,58 +1,49 @@
-/* Miller Rabin Primality Test for <= 10^18 */
-
-#define ll long long
-
-ll mulmod(ll a, ll b, ll c)
-{
-    ll x = 0, y = a % c;
-    while (b)
-    {
-        if (b & 1) x = (x + y) % c;
-        y = (y << 1) % c;
-        b >>= 1;
+#define inf (1LL << 61)
+ 
+using namespace std;
+ 
+ 
+using u64 = uint64_t;
+using u128 = __uint128_t;
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u128)result * base % mod;
+        base = (u128)base * base % mod;
+        e >>= 1;
     }
-    return x % c;
+    return result;
 }
-
-ll fastPow(ll x, ll n, ll MOD)
-{
-    ll ret = 1;
-    while (n)
-    {
-        if (n & 1) ret = mulmod(ret, x, MOD);
-        x = mulmod(x, x, MOD);
-        n >>= 1;
+bool check_composite(u64 n,u64 a,u64 d,int s) {
+    u64 x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
+            return false;
     }
-    return ret % MOD;
-}
-
-const int a[9] = { 2, 3, 5, 7, 11, 13, 17, 19, 23 };
-bool isPrime(ll n)
-{
-    if(n == 2 || n == 3) return true;
-    if(n == 1 || !(n & 1)) return false;
-    ll d = n - 1;
-    int s = 0;
-    while (d % 2 == 0)
-    {
-        s++;
-        d /= 2;
+    return true;
+};
+ 
+bool isPrime(u64 n) {
+    if (n < 2)
+        return false;
+    int r = 0;
+    u64 d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        r++;
     }
-
-    for(int i = 0; i < 9; i++)
-    {
-        if(n == a[i]) return true;
-        bool comp = fastPow(a[i], d, n) != 1;
-        if(comp) for(int j = 0; j < s; j++)
-            {
-                ll fp = fastPow(a[i], (1LL << (ll)j)*d, n);
-                if (fp == n - 1)
-                {
-                    comp = false;
-                    break;
-                }
-            }
-        if(comp) return false;
+    vector <int> v32 = {2, 3, 5, 7};
+    vector <int> v64 = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    for (int a : v64) {
+        if (n == a)
+            return true;
+        if (check_composite(n, a, d, r))
+            return false;
     }
     return true;
 }
